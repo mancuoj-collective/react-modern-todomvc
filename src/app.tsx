0e88da3e9d66b013aa0cd00f07b52b7b21240a1a
mancuoj <mancuoj@gmail.com>
2024-03-02 02:35:28 +0800

    refactor: reorganize directory structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
-import TodoInput from "./todo-input"
-import TodoList from "./todo-list"
-import TodoFooter from "./todo-footer"
-import TodoProvider from "./todo-provider"
+import TodoInput from "./components/todo-input"
+import TodoList from "./components/todo-list"
+import TodoFooter from "./components/todo-footer"
+import TodoProvider from "./components/todo-provider"
 
 export default function App() {
   return (
